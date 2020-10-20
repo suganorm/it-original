9cd489eb1fe6060f6a72dec1817782277003d2e9
suganorm <73138626+suganorm@users.noreply.github.com>
2020-10-20 13:57:36 +0300

    1

---
+++ js/translater.js
@@ -0,0 +1,278 @@
+var new_lang = {
+
+"lt0": "prodotto naturali che stabilizza<br>il livello di zucchero nel sangue",
+
+"lt1": "Chiamata gratuita",
+
+"lt2": "Stabilizza",
+
+"lt3": "il livello di zucchero nel sangue",
+
+"lt4": "Vecchio prezzo <span class=\"price_old\">50 euro.</span>",
+
+"lt5": "Prezzo con lo sconto <span class=\"price_main\">25 euro.</span>",
+
+"lt6": "Ordinare",
+
+"lt7": "Risultato dopo un ciclo di assunzione",
+
+"lt8": "Assolutamente sicuro senza effetti collaterali",
+
+"lt9": "Aiuta a differenziare le razioni alimentari",
+
+"lt10": "Aiuto e consulenza dei nostri specialisti<br>durante tutto il ciclo di assunzione",
+
+"lt11": "Ordinare con spedizione gratuita",
+
+"lt12": "Ricevere una consulenza gratuita",
+
+"lt13": "Vecchio prezzo:",
+
+"lt14": "Nuovo prezzo:",
+
+"lt15": "Inserite i vostri dati nel modulo sottostante",
+
+"lt16": "Ordinare con lo sconto",
+
+"lt17": "Consulenza gratuita",
+
+"lt18": "Prima dell’acquisto senza alcun impegno",
+
+"lt19": "Ogni giorno un nostro specialista aiuta tra i 20 e i 30 clienti che usano «SugaNorm» per domande riguardanti alimentazioni e assunzione del prodotto. Per dare la possibilità a tutti di ricevere le consulenze abbiamo nettamente aumentato il numero di specialisti certificati riguardo il preparato.",
+
+"lt20": "Oggi rimangono",
+
+"lt21": "consulenze",
+
+"lt23": "Che risultati abbiamo raggiunto",
+
+"lt24": "Stabilizzazione del livello di glucosio nel sangue in 2 settimane di assunzione",
+
+"lt25": "L’87% delle persone ha raggiunto risultati visibili dopo un mese di utilizzo",
+
+"lt26": "Più del 90% dei clienti notano che l’effetto rimane nel tempo",
+
+"lt27": "Si risparmia sulle spese mediche fino al 70%",
+
+"lt28": "Con SugaNorm<br>è più facile raggiungere risultati, iniziate ora!",
+
+"lt29": "La via per la salute con SugaNorm",
+
+"lt30": "La via della salute",
+
+"lt31": "Ordina ora in Italia",
+
+"lt33": "Che sintomi sono presenti col diabete",
+
+"lt34": "Stanchezza cronica",
+
+"lt35": "Calo progressivo di vista",
+
+"lt36": "Cambio di peso repentino",
+
+"lt37": "Le ferite non si chiudono",
+
+"lt38": "Problemi sessuali",
+
+"lt39": "Perdita di sensibilità su mani e gambe",
+
+"lt40": "Secchezza delle fauci",
+
+"lt41": "Problemi frequenti ad urinare",
+
+"lt42": "Fame continua",
+
+"lt43": "Emicrania e mal di testa",
+
+"lt44": "Causa: alto livello di zucchero nel sangue",
+
+"lt46": "Stabilizza il livello dello zucchero nel sangue",
+
+"lt47": "Libera dalla stanchezza cronica",
+
+"lt48": "Blocca il calo di vista",
+
+"lt49": "Stabilizza il peso",
+
+"lt50": "Avvia la cura dei tessuti",
+
+"lt51": "Ridona potenza sessuale",
+
+"lt52": "Stabilizza la circolazione sanguigna negli arti",
+
+"lt53": "Elimina la sete e la fame continua",
+
+"lt54": "Stabilizza il lavoro dell’apparato urinale",
+
+"lt55": "Elimina la fame continua",
+
+"lt56": "Previene i mal di testa",
+
+"lt57": "Prima si stabilizza il livello del sangue, meno sarà necessario assumere medicine, iniziate ora!!",
+
+"lt58": "SugaNorm è il preparato<br>per combattere il diabete",
+
+"lt59": "SCONTO",
+
+"lt63": "La quantità di preparato disponibile<br>nel magazzino a vi più vicino<br>è limitata!",
+
+"lt66": "Ordinare ora in Italia",
+
+"lt68": "Cosa accadrà se non si cura il diabete",
+
+"lt69": "Nefropatia",
+
+"lt70": "Angiopatia",
+
+"lt71": "Polineuropatia",
+
+"lt72": "Retinopatia",
+
+"lt73": "Chetoacidosi",
+
+"lt74": "<strong>Nefropatia</strong>— le lesioni diabetiche comportano lesioni agli elementi che filtrano i reni (glomeruli e tuboli) e ai vasi che li alimentano. La nefropatia diabetica è una delle cause più comuni di mortalità precoce e disabilità dei pazienti. Il diabete non è l'unica causa di problemi renali. Infatti tra quelli che si sottopongono alla dialisi e stanno in fila per attendere un donatore per un trapianto di rene, la maggior parte sono diabetici.",
+
+"lt75": "<strong>Angiopatia</strong> è una complicazione del diabetiche che presenta lesioni dei vasi sanguigni dell’organismo umano. Con l’effetto di «alti valori di zucchero», che viaggiano nei vasi le pareti delle arterie, delle vene e dei capillari si allargano. Come conseguenza compare l’ipoxia (carenza di ossigeno) dei tessuti circostanti e molti organi del malato vengono danneggiati. L’angiopatia di stadio avanzato comporta ulcere, necrosi dei tessuti (cancrena) e l’amputazione degli arti. In caso di ignoramento delle raccomandazioni del dottore la cancrena si sviluppa in 5 anni dopo l’inizio dell’angiopatia nel 90% dei pazienti con questa patologia.",
+
+"lt76": "<strong>Polineuropatia</strong>- Danneggiamento di diversi nervi periferici compare sotto forma di paralisi isolate, problemi di sensibilità e altri problemi, che causano forti dolori e problemi alle parti finali soprattutto degli arti. Negli stadi finali della malattia la polineuropatia degli arti può comportare l’atrofia dei muscoli che comportano ulcere trofiche e paralisi delle gambe e poi dei muscoli respiratori.",
+
+"lt77": "<strong>Retinopatia</strong>— Danneggiamento dei vasi sanguigni legati alla retina oculare. Complicazione grave e molto comune del diabete, che può comportare la cecità. Le complicazioni legate alla vista compaiono nell’85% dei malati di diabete. Quando diagnosticano il diabete di secondo tipo a persone di età media e avanzata, più del 50% dei malati presentano danneggiamenti ai vasi che irrorano l’occhio di sangue.  Si tratta della causa principale della comparsa di cecità in persone malate di diabete tra i 20 e i 74 anni.",
+
+"lt78": "<strong>Chetoacidosi</strong>— grave complicazione che si sviluppa per via della mancanza di insulina. Le cellule non possono utilizzare il glucosio come fonte di energia, e l’organismo inizia a nutrirsi attraverso le riserve grasse. Durante il consumo di grasso si sviluppano attivamente i corpi chetonici (chetoni). Quando i chetoni sono troppi iniziano a circolare nel sangue e i reni non riescono ad eliminarli per tempo dall’organismo. L’organismo inizia ad essere stanco e si può finire in coma. Questa situazione richiede un intervento medico e può comportare conseguenze letali.",
+
+"lt79": "Non perdete tempo — tenete al sicuro la vostra salute",
+
+"lt81": "Aiuta a prevenire le conseguenze",
+
+"lt82": "<strong>Previene la nefropatia</strong>— grazie al recupero del metabolismo dei carboidrati dell’organismo scompare la necessità da parte dei reni di prendere lo sforzo per sé per eliminare il glucosio dall’organismo.",
+
+"lt83": "<strong>Previene l’angiopatia</strong>— dato l’abbassamento del livello di zucchero nel sangue i vasi sono più elastici e si ripristina la circolazione spontanea.",
+
+"lt84": "<strong>Previene la polineuropatia</strong>— aiuta a prevenire i danni al sistema nervoso grazie alla diminuzione dell’azione tossica del glucosio su tutti gli organi e i tessuti dell’organismo.",
+
+"lt85": "<strong>Previene la retinopatia</strong>— ripristina il nutrimento dei nervi grazie al corretto lavoro dei vasi sanguigni degli occhi.",
+
+"lt86": "<strong>Previene la chetoacidosi</strong>— favorisce un completo nutrimento delle cellule dell’organismo di glucosio, il che da la possibilità di evitare l’intossicamento dell’organismo.",
+
+"lt87": "Prima si stabilizza il livello di zucchero, meno saranno i rischi di incorrere in conseguenze pericolose, iniziate ora!",
+
+"lt88": "SugaNorm – è un preparato<br>per la lotta contro il diabete",
+
+"lt93": "La quantità di confezioni<br>presenti nel magazzino più vicino<br>sono limitati!",
+
+"lt96": "Ordinare ora in Italia",
+
+"lt98": "Come iniziare ad utilizzare SugaNorm",
+
+"lt99": "Compilate il modulo sul sito o chiamate",
+
+"lt100": "Un nostro consulente vi richiamerà",
+
+"lt101": "Analizziamo la situazione della vostra salute",
+
+"lt102": "Identifichiamo il ciclo necessario",
+
+"lt103": "La consegna avviene alla posta a voi più vicina",
+
+"lt104": "Pagamento alla consegna",
+
+"lt105": "Un nostro specialista vi accompagnerà per tutto il periodo di cura",
+
+"lt106": "Salute di ferro e vita lunga",
+
+"lt107": "Con voi lavoreranno solo professionisti",
+
+"lt108": "Alessandro Michelangeli",
+
+"lt109": "Specialista generale della società, diabetico di tipo 1, lavora nella società da più di 5 anni, ha offerto più di 1500 consulenze.",
+
+"lt110": "Ricevere una consulenza da Alessandro",
+
+"lt111": "Recensioni su Alessandro",
+
+"lt112": "Valerio Somalia, 53 anni, Padova",
+
+"lt113": "Voglio ringraziare lo specialista Michelangeli.  Una persona che conosce personalmente i problemi legati al diabete. Io sono malato da anni. Ma i problemi seri sono iniziati l’ultimo anno. Gli uomini mi capiranno: sono diventato praticamente impotente. In famiglia sono iniziati i problemi. E tutto questo sullo sfondo di continui mal di testa, nervosismo, e come conseguenza apatia. Dalla primavera di quest’anno ho iniziato ad assumere SugaNorm. E se inizialmente ero scettico, ora dopo qualche mese posso tranquillamente raccomandare l’assunzione di SugaNorm a tutti quelli che hanno visto diversi fallimenti nella lotta contro il diabete come ho fatto io. Ora in famiglia va tutto bene e posso ringraziare solo il dottor Michelangeli. A lavoro ho perfino ricevuto una promozione.",
+
+"lt114": "Arianna Ponzo, 41 anni, Como",
+
+"lt115": "Ho cercato a lungo un rimedio per stabilizzare il livello di zucchero nel sangue. Sono malato di diabete da più di 30 anni. Pressione alta, problemi di vista, problemi con reni e fegato, ora è tutto nel passato. Prendo SugaNorm da più di 6 mesi sotto il controllo del dottor Michelangeli. Non solo controlla giornalmente l’assunzione del preparato, ma mi ha anche prescritto una dieta personale grazie alla quale sono riuscita a perdere i chili di troppo.  Grazie ancora al dottor Michelangeli per aver capito il problema e per il sostegno morale nella battaglia contro questo tremendo nemico.",
+
+"lt116": "Teresa Tersigni, 36 anni, Roma ",
+
+"lt117": "Una gravidanza tardiva e problematica, un parto cesario estremo, e un coma diabetico, le ho passate tutte prima di avere la diagnosi “diabete”. La mia situazione allora poteva essere descritta così: una mamma con una malattia grave e un bambino piccolo. Sono felice che ora è tutto rimasto nel passato. Il dottor Michelangeli mi ha letteralmente salvato la vita, con SugaNorm siamo riusciti a raggiungere ottimi risultati in 4 mesi. E il mio piccolo ora ha una mamma finalmente energica.",
+
+"lt118": "Vanessa Tormarassi",
+
+"lt119": "Specialista di prima categoria, diabetico di tipo 2, lavora nella società da 4 anni, ha aiutato più di 900 clienti",
+
+"lt120": "Ricevi una consulenza con Vanessa",
+
+"lt121": "Recensioni su vanessa",
+
+"lt122": "Michele Catanzaro, 74 anni, Molfetta",
+
+"lt123": "Sono malato di diabete da più di 20 anni. Ho provato tutto quello che mi ha raccomandato il medico curante e la medicina popolare. Il risultato era lo stesso: il livello di zucchero nel sangue continuava a crescere. Mi sentivo sempre peggio. Avevo smesso di uscire per strada, non mi bastavano le forze. SugaNorm mi ha aiutato a trovare le forze. Ho conosciuto la dottoressa Tormarassi . Al giorno d’oggi ci conosciamo da più di 6 mesi e i risultati con l’assunzione di SugaNorm non smettono di stupirmi!  Lo zucchero è nella norma. Il tono vitale è nuovamente degno e non solo esco in strada nuovamente ma frequento anche di nuovo il mio amato club di scacchi. Dottoressa Tormarassi, la ringrazio dal più profondo del cuore per la sua professionalità, per la bontà e per aver capito di cosa ha bisogno il corpo umano!",
+
+"lt124": "Eleonora Pecce, 48 anni, Milano",
+
+"lt125": "Dopo un tremendo incidente ho subito diverse operazioni e come conseguenza ho ricevuto come diagnosi il diabete. Dal 2003 prendo l’insulina. Avevo meno di 40 anni e mi sentivo come se avessi già un centinaio di anni. Dato che il livello di glucosio nel sangue è salito ho iniziato a perdere la vista e oltra al diabete si è aggiunta un’altra diagnosi, si è «legata» la malattia ipertonica. Ma come si dice: se affoghi devi iniziare a nuotare, ho deciso di salvarmi. Internet mi ha aiutato. Su un sito sul tema ho letto una storia simile alla mia dove un malato di diabete ha usato SugaNorm e ha raggiunto incredibili risultati nella lotto contro la malattia. La mia «guerra» contro il diabete non si è ancora conclusa, ma grazie a SugaNorm ho iniziato nuovamente a lavorare, mi prendo cura dei nipoti e nel prossimo anno farò un viaggio in Repubblica Ceca. Sono estremamente grata alla dottoressa Tormarassi e SugaNorm per avermi donato nuovamente il «gust» di vivere!",
+
+"lt126": "Vittoria Sarri, 62 anni, Verona",
+
+"lt127": "La dottoressa Tormarassi è un professionista con la S maiuscola! Sa quello che fa, è un vero specialista. Il suo lavoro può essere portato come esempio a qualsiasi specialista. La mia istoria è simile a centinaia di altre: predisposizione genetica al diabete che ha avuto effetti negativi sull’organismo. Come un fulmine a ciel sereno nel 2007 ho ricevuto dal dottore la diagnosi: diabete Ho cambiato un medicinale dopo l’altro. Lo dirò in maniera sincera, non mi ha aiutato nulla. Stavo sempre peggio: avevo il fiatone, mi faceva male la testa, mi girava la testa e avevo la bocca secca. Una situazione insostenibile. DI SugaNorm ne ho sentito parlare per caso dall’endocrinologo. E la ragazza che me ne ha parlato non sembrava affatto diabetica rispetto agli altri. Ho deciso di provarci anche io e ho visitato il sito di SugaNorm. Mi è stato raccomandato un ciclo di 6 mesi. Oggi ho un valore di glucosio del 5.8, mi sento benissimo, tutte le analisi sono nella nosrma e voglio ringraziare la dottoressa Tormarassi e SugaNorm!",
+
+"lt128": "Giulia Pezzella",
+
+"lt129": "lavora nella società fin dalla fondazione, ha il più grande numero di pazienti.",
+
+"lt130": "Valentina Fassone",
+
+"lt131": "se ne intende più di tutti di dieta diabetica.",
+
+"lt132": "Ludovica Bevilacqua",
+
+"lt133": "specialista esperto in diversi campi.",
+
+"lt134": "Margherita Colle",
+
+"lt135": "esperto irrinunciabile per quanto riguarda le complicazioni del diabete.",
+
+"lt136": "con SugaNorm tutto è veramente più semplice",
+
+"lt137": "Sicuro e senza effetti collaterali",
+
+"lt138": "Non provoca dipendenza",
+
+"lt139": "Universale per i diabetici di entrambe i tipi",
+
+"lt140": "Economico e accessibile",
+
+"lt141": "SugaNorm – un preparato<br>per la battaglia contro il diabete",
+
+"lt146": "IL numero di confezioni<br>nel magazzino a voi più vicino<br>è limitato!",
+
+"lt151": "Servizio di richiamata",
+
+"lt152": "Compilate il modulo e un nostro specialista vi chiamerà per definire i dettagli",
+
+"lt154": "SugaNorm – è un preparato<br>per la lotta contro il diabete",
+
+"lt159": "Il numero di confezioni<br>nel magazzino a voi più vicino<br>è limitato!",
+
+"lt162": " SugaNorm – è un preparato<br>per la lotta contro il diabete ",
+
+"lt167": " Il numero di confezioni<br>nel magazzino a voi più vicino<br>è limitato!",
+
+"lt170": "Grazie per aver compilato il modulo"
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
